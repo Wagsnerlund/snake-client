a520bcd77eb4de3f4f19c6d6c65cf71b21fc8c6a
Wagsnerlund <chelseawagner17@gmail.com>
2021-03-10 23:49:00 +0000

    moving the snake

--- client.js
+++ client.js
@@ -12,15 +12,35 @@ const connect = function () {
     console.log(dataFromServer);
   })
 
-  conn.on("connect", () => {
-    console.log('Successfully connected to game server')
-  })
+  // const movement = {
+  //   up: conn.write('Move: up'),
+  //   down: conn.write('Move: down'),
+  //   left: conn.write('Move: left'),
+  //   right: conn.write('Move: right')
+  // }
 
+  const interval = 200;
   conn.on("connect", () => {
-    conn.write('Name:  CAW');
+    console.log('Successfully connected to game server');
+    conn.write('Name: CAW');
+    // conn.write('Move: up');
+    setTimeout(() => {
+      conn.write('Move: up');
+      setTimeout(() => {
+        conn.write('Move: left');
+          setTimeout(() => {
+            conn.write('Move: left');
+        }, interval);
+      }, interval);
+    }, interval);
   })
 
   return conn;
 };
 
 module.exports = connect;
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
