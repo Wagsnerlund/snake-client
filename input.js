376424b0fd31777cf86a9e0544c886f3b440ce33
Wagsnerlund <chelseawagner17@gmail.com>
2021-03-11 06:19:35 +0000

    added special snake sayings

--- input.js
+++ input.js
@@ -1,16 +1,38 @@
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  };
-}
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on("data", handleUserInput)
-  return stdin;
-}
-setupInput();
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    };
+    if (key === 'w') {
+      connection.write('Move: up');
+    };
+    if (key === 'a') {
+      connection.write('Move: left');
+    };
+    if (key === 's') {
+      connection.write('Move: down');
+    };
+    if (key === 'd') {
+      connection.write('Move: right');
+    };
+    if (key === 'r') {
+      connection.write("Says: I'm a slithery little sneaky snake!");
+    };
+    if (key === 'm') {
+      connection.write("Says: Slither in the garden, catching me a mouse!");
+    };
+  };
 
-module.exports = {setupInput};
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -2,6 +2,13 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
 
-setupInput();
+let connection = connect();
+setupInput(connection);
+
+
+// Update play.js to send the connection object returned from connect() into our setupInput() function.
+
+// Our input module can now use the connection variable to send movement commands/messages to the server.
+
+// Run the code again to ensure that it is valid and runnable, as before.
